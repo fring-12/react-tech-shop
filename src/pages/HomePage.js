782a301b4e56de90e848c92574a902259015c53f
Banedict Fring Drong <banedictfring12@gmail.com>
2020-04-20 04:03:54 +0600

    context api, sidebar, navbar, sidecart, hero sec

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <>
-      <h1>Hello From Home Page</h1>
+      <Hero title="awesome gadgets" max="true">
+        <Link to="/products">our products</Link>
+      </Hero>
     </>
   );
 }
